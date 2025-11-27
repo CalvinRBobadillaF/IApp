f81f3c323d5804ed283e69bef1ed2e3d098695ff
Calvin Bobadilla <calvinbobadillaf@gmail.com>
2025-11-26 20:31:20 -0400

    Design changes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -22,7 +22,9 @@ const Sidebar = () => {
         <div className={`sidebar ${openSidebar ? 'open' : ''}`}>
             
             <div className="top">
-                <img className="menu" onClick={() => setOpenSidebar(!openSidebar)} src={assets.menu_icon} alt="" />
+                <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setOpenSidebar(!openSidebar)} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" className="MenuPC">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+  </svg>
                 <div className="new-chat">
                 <img src={assets.plus_icon} alt="" onClick={() => newChat()} />
                 {openSidebar?<p onClick={() => newChat()}>New Chat</p>:null}
